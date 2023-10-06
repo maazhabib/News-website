444ed26f4e32b34cad795f6a701d8a5ab23ffd21
maazhabib <maazhabib73@gamil.com>
2023-10-06 10:56:03 +0500

    news

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
                           $start = ($page -1) * $per_page;
 
 
-                          $Fetch_data= "SELECT post.post_id , post.title , post.description , post.post_date ,category.category_name, user.username, post.post_img FROM post 
+                          $Fetch_data= "SELECT post.post_id , post.title , post.description , post.post_date ,category.category_name, user.username, post.post_img , post.author FROM post 
                           LEFT JOIN category ON post.category = category.category_id
                           LEFT JOIN user ON post.author = user.user_id
                           ORDER BY post.post_id DESC LIMIT $start, $per_page ";
@@ -43,7 +43,7 @@
                                             </span>
                                             <span>
                                                 <i class="fa fa-user" aria-hidden="true"></i>
-                                                <a href='author.php'><?php echo $row['username']?></a>
+                                                <a href='author.php?aid=<?php echo $row['author']?>'><?php echo $row['username']?></a>
                                             </span>
                                             <span>
                                                 <i class="fa fa-calendar" aria-hidden="true"></i>
